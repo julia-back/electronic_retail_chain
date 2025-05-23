1bd9566fa2fb494a35ff9613831f2a7df65a4ae9
Julia Bengert <yubengert@gmail.com>
2025-05-23 17:30:26 +0300

    add views, serializers, urls for user

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .apps import UsersConfig
+from . import views
+
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    # path(),
+    path("user/<int:pk>/", views.UserRetrieveAPIView.as_view(), name="user_retrieve"),
+    path("user/new/", views.UserCreateAPIView.as_view(), name="user_create"),
+    path("user/<int:pk>/", views.UserUpdateAPIView.as_view(), name="user_update"),
+    path("user/<int:pk>/", views.UserDestroyAPIView.as_view(), name="user_destroy"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from . import serializers
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = serializers.UserDetailSerializer
+    permission_classes = []
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = serializers.UserDetailSerializer
+    permission_classes = []
+
+    def perform_create(self, serializer):
+        serializer.save(
+            is_active=False,
+            is_staff=False,
+            password=make_password(serializer.validated_data.get("password"))
+        )
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = serializers.UserDetailSerializer
+    permission_classes = []
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = serializers.UserDetailSerializer
+    permission_classes = []
