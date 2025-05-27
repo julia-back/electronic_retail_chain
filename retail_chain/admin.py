e2c7bfc8cddefce4be3737161017772de86d4eef
Julia Bengert <yubengert@gmail.com>
2025-05-27 21:56:32 +0300

    add admin action clear_payment_arrears

--- retail_chain/admin.py
+++ retail_chain/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import ChainNode, Contacts
 
 
@@ -10,3 +10,9 @@ class ChainNodeAdmin(admin.ModelAdmin):
     list_display = ["name", "contacts", "supplier", "payment_arrears", "node_type", "node_level", "created_at"]
     list_filter = ["contacts__country"]
     search_fields = ["name"]
+    actions = ["clear_payment_arrears"]
+
+    @admin.action(description="Очистить поле задолженности")
+    def clear_payment_arrears(self, request, queryset):
+        queryset.update(payment_arrears=0)
+        self.message_user(request, "Задолженность очищена для выбранных объектов.", level=messages.SUCCESS)
