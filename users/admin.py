d6947302c8385f7c8490d702f8f79ccfc8c7dfd3
Julia Bengert <yubengert@gmail.com>
2025-05-27 23:14:20 +0300

    add admin action do_user_is_staff

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,13 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email", "first_name", "last_name", "username"]
+    actions = ["do_user_is_staff"]
+
+    @admin.action(description="Присваивает пользователю статус сотрудника.")
+    def do_user_is_staff(self, request, queryset):
+        queryset.update(is_staff=True)
+        self.message_user(request, "Выбранным пользователям присвоен статус сотрудника.", messages.SUCCESS)
